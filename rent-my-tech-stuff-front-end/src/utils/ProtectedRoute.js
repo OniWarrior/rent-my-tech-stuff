23c3e19e9113fc1aa905b13e80f5955583695be9
Stephen Aranda <aranda.stephen88@gmail.com>
2021-09-14 12:52:40 -0500

    created protected route component and AxiosWithAuth component

---
+++ rent-my-tech-stuff-front-end/src/utils/ProtectedRoute.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Route,Redirect} from 'react-router'
+
+const ProtectedRoute=({component,...rest})=>{
+    if(localStorage.getItem("token")){
+        return (<Route component={component} {...rest} />)
+    }
+    else{
+        return <Redirect to='/Login'/>
+    }
+
+}
+
+export default ProtectedRoute
